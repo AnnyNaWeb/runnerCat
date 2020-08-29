3b0f540f886a39a1919f805a6ba468350ed71727
annynaweb <sousa.annyc@gmail.com>
2020-08-29 12:41:16 -0300

    Parallax do Chao

---
+++ Assets/Scripts/parallax.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+
+    public float speedText;
+    void Update(){
+        float offset = Time.time * speedText;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset,0 ));
+    }
+
+}
