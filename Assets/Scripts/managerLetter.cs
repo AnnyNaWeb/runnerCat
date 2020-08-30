f942bfb9de305552eae5f4380ac5f4839599d402
annynaweb <sousa.annyc@gmail.com>
2020-08-30 00:16:10 -0300

    Carta 1 implementada

---
+++ Assets/Scripts/managerLetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class managerLetter : MonoBehaviour {
+
+    public GameObject LetterOne;
+    public GameObject ButtonOne;
+    public GameObject ButtonBack;
+
+    public void exibeCarta () {
+        LetterOne.SetActive (true);
+        ButtonOne.SetActive (false);
+        ButtonBack.SetActive (true);
+    }
+
+    public void escondeCarta () {
+        LetterOne.SetActive (false);
+        ButtonOne.SetActive (true);
+        ButtonBack.SetActive (false);
+    }
+}
--- Assets/Scripts/receiveLetter.cs
+++ Assets/Scripts/receiveLetter.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class receiveLetter : MonoBehaviour
-{
+public class receiveLetter : MonoBehaviour {
 
-    public int letterNumber;
+    public static int letterNumber;
 
     SpriteRenderer spr;
+    public GameObject button;
 
     public Sprite newSprite;
 
-    void Start()
-    {
-        spr = GetComponent<SpriteRenderer>();
+    void Start () {
+        spr = GetComponent<SpriteRenderer> ();
 
-        Debug.Log("Mail Number: " + (letterNumber == (PlayerPrefs.GetInt("Collection"))));
-        Debug.Log("letterNumber: " + letterNumber);
+        Debug.Log ("Mail Number: " + (letterNumber == (PlayerPrefs.GetInt ("Collection"))));
+        Debug.Log ("letterNumber: " + letterNumber);
 
-        if (letterNumber <= (PlayerPrefs.GetInt("Collection")))
-        {
+        if (letterNumber <= (PlayerPrefs.GetInt ("Collection"))) {
             spr.sprite = newSprite;
+            button.SetActive (true);
+        } else {
+            button.SetActive (false);
         }
     }
+
 }
