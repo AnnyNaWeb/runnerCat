0b96becb35ff08de19a7f8618a68da8440faf056
annynaweb <sousa.annyc@gmail.com>
2020-08-30 17:36:09 -0300

    Audio implementado

---
+++ Assets/Scripts/som.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class som : MonoBehaviour {
+    private static som instance;
+    void Awake () {
+        if (instance != null) {
+            Destroy (gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad (transform.gameObject);
+        }
+
+    }
+}
